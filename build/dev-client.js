857e9737e5d7ae4837d6adf6b2fd3b77e68934d5
hazyzh <532599877@qq.com>
2018-01-27 21:29:47 +0800

    更新依赖文件和正式打包插件修改
    增加了transform-react-remove-prop-types插件 正式环境去除prop-types相关文件

--- build/dev-client.js
+++ build/dev-client.js
@@ -4,7 +4,6 @@ require('eventsource-polyfill')
 var hotClient = require('webpack-hot-middleware/client?noInfo=false&reload=true')
 
 hotClient.subscribe(function (event) {
-  console.log(event)
   if (event.action === 'reload') {
     window.location.reload()
   }
